4cb8df4718bc3b10c05e4bcf60db586ad929ed49
Yuta K <ugwis@icloud.com>
2016-05-12 21:26:21 +0900

    Update master.blade.php

--- kstmctf/resources/views/master.blade.php
+++ kstmctf/resources/views/master.blade.php
@@ -10,7 +10,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	</head>
-	<body style="padding-top:50px;">
+	<body style="padding-top:70px;">
 		<nav class="navbar navbar-default navbar-fixed-top">
 		<div class="container">
 		<div class="navbar-header">
